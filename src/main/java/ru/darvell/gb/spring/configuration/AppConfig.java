123fc0a7683dcaa869b04ecfcdf50ee12605ab58
Andrey Dyakov <dyakov.as@esphere.ru>
2021-08-17 12:19:05 +0300

    cleanup

--- src/main/java/ru/darvell/gb/spring/configuration/AppConfig.java
+++
@@ -1,9 +0,0 @@
-package ru.darvell.gb.spring.configuration;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan({"ru.darvell.gb.spring"})
-public class AppConfig {
-}
